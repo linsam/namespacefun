9e07ef28158a484c2c18d18874c1729859b1a1ed
John O'Meara <john.fr.omeara@gmail.com>
2016-01-21 00:00:27 -0500

    Add comments about gid.

--- pid.c
+++ pid.c
@@ -24,11 +24,40 @@ static int setupIdMaps() {
     sprintf(buf, "0 1000 1\n");
     ssize_t s = write(fd, buf, strlen(buf));
     if (s != strlen(buf)) {
-        perror("write");
+        perror("write uid");
         close(fd);
         return -2;
     }
     close(fd);
+
+    /* TODO: Newer (3.19 I think) kernels require user programs to write
+     * setgroups to false before setting the gid_map. Older kernels don't
+     * have this file, and also allow the program to call setgroups to drop
+     * groups before writing the group map, which turns out to be a
+     * security problem. Unfortunately, in all kernels to date
+     * (2016-01-15), you can only map a single group to your GID, and
+     * cannot map anything to your supplemental GIDs (unless you are
+     * priveleged outside of your namespace). This means that, unless you
+     * run an older kernel AND drop your groups, you end up in your
+     * namespace with a bunch of supplemental groups that all have the same
+     * ID number (equal to the overflow gid number).
+     *
+     * The TODO here is primarily that we only work on the older kernels
+     * with the below commented out, or only the newer ones (probably) with
+     * it in. We ought to work on both (try setgroups, ignore failure).
+     * Either way we shouldn't drop groups unless we are privileged outside
+     * the namespace, or also ignore that error.
+     */
+#if 0
+    sprintf(buf, "/proc/%u/setgroups", getpid());
+    fd = open(buf, O_WRONLY);
+    if (fd < 0) {
+        perror(buf);
+        return fd;
+    }
+    write(fd, "deny\n", 5);
+    close(fd);
+#endif
 
     sprintf(buf, "/proc/%u/gid_map", getpid());
     fd = open(buf, O_WRONLY);
@@ -37,9 +66,10 @@ static int setupIdMaps() {
         return fd;
     }
     sprintf(buf, "5 1000 1\n");
+    //sprintf(buf, "5 1000 1\n4 4 1\n");
     s = write(fd, buf, strlen(buf));
     if (s != strlen(buf)) {
-        perror("write");
+        perror("write gid");
         close(fd);
         return -2;
     }
@@ -71,10 +101,12 @@ static int child_fn() {
     mount("proc", "/proc", "proc", 0, "");
     sethostname("pidtest", 7);
     /* Setup a user id mapping */
+    printf("c:You are %i (acting as %i)\n", getuid(), geteuid());
     if (setupIdMaps()) {
         printf("Child: Error setting user id mapping\n");
         return 1;
     }
+    printf("c:You are %i (acting as %i)\n", getuid(), geteuid());
     /* Remove all supplementary groups */
     //setgroups(0, NULL);
 #if 0 /* can't drop privilege if single mapping exists. We are always exactly the single ID we put into the uid_map file. */
@@ -89,6 +121,7 @@ static int child_fn() {
     return 0;
 }
 int main() {
+    printf("You are %i (acting as %i)\n", getuid(), geteuid());
     printf("Cloning\n");
     pid_t child = clone(child_fn, child_stack+1024*1024, CLONE_NEWPID | SIGCHLD | CLONE_NEWNS | CLONE_NEWNET | CLONE_NEWUSER | CLONE_NEWUTS, NULL);
     if (child == -1) {
